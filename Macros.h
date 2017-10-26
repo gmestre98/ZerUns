b178d1bd836f3a13b8bf1d590f65e975b0d1dcf4
GuiGuerreiro <guilherme.guerreiro@tecnico.ulisboa.pt>
2017-10-26 17:33:22 +0100

    Alterações na leitura

--- Macros.h
+++ Macros.h
@@ -2,3 +2,17 @@
 #include <stdio.h>
 #define PRINTERROR(...) ( fprintf(stderr,__VA_ARGS__));
 #define CRASH exit(0);
+
+typedef struct Puzzle{
+	int size;
+	int line;
+	int col;
+	int binary;
+	int **matrix;
+	int result;
+} Puzzle;
+
+typedef struct PuzzleNode{
+	struct Puzzle content;
+	struct Puzzle *next;
+} PuzzleNode;
