5d6088126ba3ba031c999a8931f531413e3c9df7
gmestre98 <goncalo.mestre@tecnico.ulisboa.pt>
2017-10-27 09:15:31 -0400

    Files with extra comments

--- Macros.h
+++ Macros.h
@@ -1,4 +1,31 @@
+/******************************************************************************
+ *
+ * File Name: Macros.h
+ * Author:    Gonçalo Mestre nº 87005  &  Guilherme Guerreiro nº87010
+ * Revision:  26 Oct 2017
+ *
+ * NAME
+ *  Macros - Important definitions
+ *
+ * DESCRIPTION
+ *  File containing the important Macros definitions for the project
+ *
+ *****************************************************************************/
 #include <stdlib.h>
 #include <stdio.h>
 #define PRINTERROR(...) ( fprintf(stderr,__VA_ARGS__));
 #define CRASH exit(0);
+
+typedef struct Puzzle{
+    int size;
+    int line;
+    int col;
+    int binary;
+    int **matrix;
+    int result;
+}Puzzle;
+
+typedef struct PuzzleNode{
+    struct Puzzle content;
+    struct Puzzle* next;
+}PuzzleNode;
